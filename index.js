e447b7e0bb368fef207a34848c1c22dcc342d3a6
Moradi1412 <moradi.javid@gmail.com>
2021-09-12 00:17:28 -0700

    added all requested userinput and console.log to ensure getting the data

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const userPrompt = () => {
     return inquirer.prompt([
         {
             type: 'input',
-            name: 'gitbubUsername', 
+            name: 'githubUsername', 
             message: "What is your GitHub username ? ",
             validate: userNameInput => {
             if (userNameInput) {
@@ -20,14 +20,50 @@ const userPrompt = () => {
         {
             type: 'input',
             name: 'emailAddress', 
-            message: "what is your email address ",
-        }
+            message: "What is your email address? "
+        },
+        {
+            type: 'input',
+            name: 'projectName', 
+            message: "What is your project name? "
+        },
+        {
+            type: 'checkbox',
+            name: 'licenses', 
+            message: "What kind of license your projectr should have ? ",
+            choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]
+        },
+        {
+            type: 'input',
+            name: 'runCommand', 
+            message: "What command should be run to install dependencies?  ", 
+            default: "npm install"
+        },
+        {
+            type: 'input',
+            name: 'runTest', 
+            message: "What command should be run to test", 
+            default: "npm test"
+        },
+        {
+            type: 'input',
+            name: 'repoInfo', 
+            message: "What does the user need to know about using the repo", 
+        },
+        {
+            type: 'input',
+            name: 'contributing', 
+            message: "What does the user need to know about contributing to the repo", 
+        },
 
     ]); 
 }; 
 
 
-
+userPrompt()
+    .then(userInput => {
+        console.log(userInput)
+    })
 
 
 

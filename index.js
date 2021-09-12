8c20110df4bd967474db8ed4282506d005c5a9d3
Moradi1412 <moradi.javid@gmail.com>
2021-09-12 00:04:50 -0700

    intialing first user input

--- index.js
+++ index.js
@@ -1,13 +1,50 @@
-// TODO: Include packages needed for this application
+// Including packages needed for this application
+//inquirer to get the data from terminal 
+const inquirer = require('inquirer');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const userPrompt = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'gitbubUsername', 
+            message: "What is your GitHub username ? ",
+            validate: userNameInput => {
+            if (userNameInput) {
+                return true; 
+            } else {
+                console.log("please enter your username! ")
+                return false; 
+            }
+            }
+        }, 
+        {
+            type: 'input',
+            name: 'emailAddress', 
+            message: "what is your email address ",
+        }
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+    ]); 
+}; 
 
-// TODO: Create a function to initialize app
-function init() {}
 
-// Function call to initialize app
-init();
+
+
+
+
+
+
+
+
+
+
+// // TODO: Create an array of questions for user input
+// const questions = [];
+
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
+
+// // TODO: Create a function to initialize app
+// function init() {}
+
+// // Function call to initialize app
+// init();
